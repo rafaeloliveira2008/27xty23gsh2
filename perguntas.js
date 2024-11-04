c9cc2443b68226fea48f083fa0e178ff95e51501
rafaeloliveira2008 <144123576+rafaeloliveira2008@users.noreply.github.com>
2024-11-04 12:13:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Barriguinha Mole',
+    'Quem é o Barriguinha Mole   ?',
+    '  Barriguinha Mole como é vulgarmente conhecido, representa uma marca de leite na região acreana (tinha que ser no acre), e é considerada por muitos a pior melhor publicidade de TODOS OS TEMPOS',
+)
+
+criaCartao(
+    'Barriguinha Mole',
+    ' História',
+    'A publicidade foi invejada por diversas empresas, incluindo nelas a ex-melhor de todas: Pizza Pixar que inclusive pensou em contratar imediatamente o diretor e animador por trás do simples comercial, porém sem sucesso. ',
+)
+
+criaCartao(
+    'Barriguinha Mole',
+    '  Fato?',
+    'Sem contar que ele é nosso amiguinho!'
+)
+
+criaCartao(
+    ' Barriguinha Mole',
+    'Qualo Seu Maior Concorrente',
+    '  Dollynho'
+)
+
+criaCartao(
+    'Barriguinha Mole ',
+    'Rumores',
+    '   Rumores dizem que toda a animação foi feita no Paint.'
+)
